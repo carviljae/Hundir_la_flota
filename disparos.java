f904b4d2820af4cd6147f411a6f152cd73e2f008
nelbaeesc <nelbaeesc@alu.edu.gva.es>
2022-11-15 20:42:02 +0100

    halo

--- disparos.java
+++ disparos.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 public class disparos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String coordenadas;
+        boolean disparo=false;
+        char letra='A';
+        int num=1;
 
         System.out.println("Dime las coodenadas: ");
+        coordenadas = sc.next();
+        while (disparo)
+            if (coordenadas.charAt(0) == letra && coordenadas.charAt(1) == num){
 
-
-
+            }
     }
-   // public static boolean disparoJugador (char[][] tableroDisparosJugador, char[][] tableroPC){
-
-
-
-   // }
-
-
-
 }
