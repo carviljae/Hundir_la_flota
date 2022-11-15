b8c2714a69ad0cd27fd76feaaa2ee7b051efcdb4
carviljae <carviljae@ale.edu.gva.es>
2022-11-15 20:33:19 +0100

    hola

--- src/disparos.java
+++ src/disparos.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class disparos {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
 
         System.out.println("Dime las coodenadas: ");
 
