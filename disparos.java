71a7abfa197b4257ccb490cc4a832a9486022e7f
carviljae <carviljae@ale.edu.gva.es>
2022-11-15 20:37:22 +0100

    hola

--- disparos.java
+++ disparos.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class disparos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int fila;
-        int col;
-        int[][] disparos = new int[fila][col];
+
 
         System.out.println("Dime las coodenadas: ");
 
