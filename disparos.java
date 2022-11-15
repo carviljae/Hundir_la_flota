336c185ed2a7ede5cf56fc5a08d5a3b7e6ebbe98
carviljae <118199612+carviljae@users.noreply.github.com>
2022-11-15 19:31:36 +0100

    Add files via upload

---
+++ disparos.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class disparos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int fila;
+        int col;
+        int[][] disparos = new int[fila][col];
+
+        System.out.println("Dime las coodenadas: ");
+
+
+
+    }
+   // public static boolean disparoJugador (char[][] tableroDisparosJugador, char[][] tableroPC){
+
+
+
+   // }
+
+
+
+}
