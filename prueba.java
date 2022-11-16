834d2e8301381c532daad4a621100877e2af9a6e
carviljae <carviljae@ale.edu.gva.es>
2022-11-16 20:34:29 +0100

    hola2

--- prueba.java
+++ prueba.java
@@ -8,13 +8,12 @@ public class prueba {
         System.out.println("Introduce una coordenada: ");
         coordenadas = sc.next();
 
-        if (coordenadas.length()==2 && coordenadas.charAt(0) == 'A'+ tablero.length  ){
-            correcto=true;
-            System.out.println("coordenada valida");
-        }
-        else {
+        if(Character.isLetter(coordenadas.charAt(0)) && Character.isDigit(coordenadas.charAt(1))&&coordenadas.length()==2){
+            correcto = true;
+            System.out.println("coordenada  valida");
+        }else{
             correcto = false;
-            System.out.println("coordenada no valida");
+            System.out.println("coordenada  no valida");
         }
 
         return correcto;
